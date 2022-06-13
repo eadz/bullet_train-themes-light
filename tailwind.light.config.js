c376a704873119a27f457fbb278b3bec79646686
Eaden McKee <_@eaden.net>
2022-06-13 19:52:43 +1200

    no night mode

--- tailwind.light.config.js
+++ tailwind.light.config.js
@@ -12,7 +12,6 @@ module.exports = {
     './tmp/gems/*/app/assets/stylesheets/**/*.css',
     './tmp/gems/*/app/javascript/**/*.js',
   ],
-  darkMode: 'media',
   theme: {
     extend: {
       fontSize: {
